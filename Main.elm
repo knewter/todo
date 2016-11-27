485416bb6654ccdde6f1bae65e9f0997665a8a9a
Josh Adams <josh.rubyist@gmail.com>
2016-11-27 02:43:57 -0600

    Fix bug where we weren't updating the newTodo identifier

--- Main.elm
+++ Main.elm
@@ -66,7 +66,7 @@ update msg model =
         Add ->
             { model
                 | todos = model.todo :: model.todos
-                , todo = newTodo
+                , todo = { newTodo | identifier = model.nextIdentifier }
                 , nextIdentifier = model.nextIdentifier + 1
             }
 
